7976506778110013752bdc3aa9a38a9672cb75a7
Dhanush Gowda H K <dhanushgowdahk04@gmail.com>
2025-10-24 14:31:45 +0530

    Solve Valid Anagram problem

---
+++ DSA/Leetcode/ValidAnagram.java
@@ -0,0 +1,24 @@
+public class ValidAnagram {
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        System.out.println(isAnagram(s, t));
+    }
+
+    static boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        int[] charCount = new int[26];
+
+        for(int i=0; i<s.length(); i++){
+            charCount[s.charAt(i) - 'a']++;
+            charCount[t.charAt(i) - 'a']--;
+        }
+
+        for(int count: charCount){
+            if(count != 0) return false;
+        }
+
+        return true;
+    }
+}
