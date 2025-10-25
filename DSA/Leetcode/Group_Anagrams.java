8376d80fd162ba476a88b559fbd42a24af997f6c
Dhanush Gowda H K <dhanushgowdahk04@gmail.com>
2025-10-25 11:18:54 +0530

    Solve Group Anagram Problem

---
+++ DSA/Leetcode/Group_Anagrams.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Group_Anagrams {
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+
+    static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<>());
+            }
+            map.get(sortedStr).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
--- DSA/Leetcode/TwoSum.java
+++ DSA/Leetcode/TwoSum.java
@@ -4,24 +4,26 @@ import java.util.Map;
 
 public class TwoSum {
     public static void main(String[] args) {
-        int[] arr = {15, 7, 3, 2};
+        int[] arr = {2, 7, 11, 15};
         int target = 9;
-        System.out.println(Arrays.toString(twoSum(arr, target)));
+        
+        int[] result = twoSum(arr, target);
+        System.out.println(Arrays.toString(result)); // Output: [0, 1]
     }
-
-    static int[] twoSum(int[] arr, int target){
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for(int i=0; i<arr.length; i++){
-            int compliment = target - arr[i];
-
-            if(map.containsKey(compliment)){
-                return new int[]{map.get(compliment), i};
-            }
-
-            map.put(arr[i], i);
-        }
-
-        return new int[]{};
-    }
+    
+	static int[] twoSum(int[] arr, int target){
+		Map<Integer, Integer> map = new HashMap<>();
+		
+		for(int i=0; i<arr.length; i++){
+			int complement = target - arr[i];
+			
+			if(map.containsKey(complement)){
+				return new int[]{map.get(complement), i};
+			}
+			
+			map.put(arr[i], i);
+		}
+		
+		return new int[]{};
+	}
 }
